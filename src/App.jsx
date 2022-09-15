2fb7c102f5ddb6328e0e6208dcdc41e3ce05900f
martsDev <martssilvapro@gmail.com>
2022-09-15 15:08:21 -0300

    customize movie grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
 import Movie from './components/Movie';
+
 
 
 const App = () => {
@@ -18,8 +20,8 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <div>
+    <div className='app'>
+      <div className='popular-movies'>
         {popular.map((movie) => (
           <Movie key={ movie.id } movie={ movie } />
         ))}
